3270d2f0fc99f4f3f9959a6e2937316a10262a49
YogeshDekate25 <sonudekate748@gmail.com>
2023-02-08 11:39:24 +0530

    Taking Input

--- Day1.cpp
+++ Day1.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 int main()
 {
-    cout<<"Hello, Good Morning";
+    cout<<"Hello, Good Morning\n";
+    //claculating total amount after paying using Userinput
+     int totalamount;
+     int amountpaid;
+
+    cout<<"The total amount you have : ";
+    cin>>totalamount;
+
+    cout<<"The amount to be paid : ";
+    cin>>amountpaid;
+
+    int amountleft = totalamount - amountpaid;
+
+    cout<<amountleft; 
     return 0;
 }
