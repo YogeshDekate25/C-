9d1487cb66b1fbc2f6fc48cc6f8cf42726424353
YogeshDekate25 <sonudekate748@gmail.com>
2023-02-15 10:42:10 +0530

    Factorial of a Number

--- Day3.cpp
+++ Day3.cpp
@@ -16,13 +16,25 @@ void Fib(int n)
         t2=nextTerm;
     }
        return;
-}
+  }
+
+  // Function for calculating Factorial of a number  
+
+  int fact(int n){
+       int factorial=1;
+       for(int i=2 ; i<=n ; i++){
+          factorial*=i;
+       }
+       return factorial;
+  }   
+
     int main()
     {    
         int n;
         cin>>n;
 
-        Fib(n);
+        int ans = fact(n);
+        cout<<ans<<endl;
 
         return 0;
     }
